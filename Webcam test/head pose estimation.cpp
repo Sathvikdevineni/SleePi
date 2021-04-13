7b45cad9bd50e547e86c688ba0e7a3635f1fe8e0
Sathvikdevineni <73529936+Sathvikdevineni@users.noreply.github.com>
2021-04-13 23:39:58 +0100

    Implemented 2d image points HPE
    added the 2d image points in to the head pose estimator to respective corner points

--- Webcam test/head pose estimation.cpp
+++ Webcam test/head pose estimation.cpp
@@ -5,7 +5,17 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
+      // Read input image
+    cv::Mat im = cv::imread("headPose.jpg");
     
+    // 2D image points are described in it
+    std::vector<cv::Point2d> image_points;
+    image_points.push_back( cv::Point2d(359, 391) );    // Nose tip
+    image_points.push_back( cv::Point2d(399, 561) );    // Chin
+    image_points.push_back( cv::Point2d(337, 297) );     // Left eye left corner
+    image_points.push_back( cv::Point2d(513, 301) );    // Right eye right corner
+    image_points.push_back( cv::Point2d(345, 465) );    // Left Mouth corner
+    image_points.push_back( cv::Point2d(453, 469) );    // Right mouth corner
     
 
 }
