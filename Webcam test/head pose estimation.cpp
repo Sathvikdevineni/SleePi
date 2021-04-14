397ee37177d7204bf3fd693238adcb60b825fc74
Sathvikdevineni <73529936+Sathvikdevineni@users.noreply.github.com>
2021-04-14 12:49:14 +0100

    added the 3d model points to HPE
    Added the 3d model points to the code for the representation in image co-ordinate system

--- Webcam test/head pose estimation.cpp
+++ Webcam test/head pose estimation.cpp
@@ -5,19 +5,26 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
-      // Read input image
+    // Read input image
     cv::Mat im = cv::imread("headPose.jpg");
     
-    // 2D image points are described in it
-    std::vector<cv::Point2d> image_points;
-    image_points.push_back( cv::Point2d(359, 391) );    // Nose tip
-    image_points.push_back( cv::Point2d(399, 561) );    // Chin
-    image_points.push_back( cv::Point2d(337, 297) );     // Left eye left corner
-    image_points.push_back( cv::Point2d(513, 301) );    // Right eye right corner
-    image_points.push_back( cv::Point2d(345, 465) );    // Left Mouth corner
-    image_points.push_back( cv::Point2d(453, 469) );    // Right mouth corner
-    std::vector<cv::Point3d> model_points;
-    model_points.push_back(cv::Point3d(0.0f, 0.0f, 0.0f)); // added the 3d point for nose tip
+    // 2D image points. If you change the image, you need to change vector
+    std::vector<cv::Point2d> Image_Points;
+   Image_Points.push_back( cv::Point2d(359, 391) );    // Nose tip
+   Image_Points.push_back( cv::Point2d(399, 561) );    // Chin
+   Image_Points.push_back( cv::Point2d(337, 297) );     // Left eye left corner
+   Image_Points.push_back( cv::Point2d(513, 301) );    // Right eye right corner
+   Image_Points.push_back( cv::Point2d(345, 465) );    // Left Mouth corner
+   Image_Points.push_back( cv::Point2d(453, 469) );    // Right mouth corner
+    
+    // 3D model points.
+    std::vector<cv::Point3d> Model_Points;
+    Model_Points.push_back(cv::Point3d(0.0f, 0.0f, 0.0f));               // Nose tip
+    Model_Points.push_back(cv::Point3d(0.0f, -330.0f, -65.0f));          // Chin
+    Model_Points.push_back(cv::Point3d(-225.0f, 170.0f, -135.0f));       // Left eye left corner
+    Model_Points.push_back(cv::Point3d(225.0f, 170.0f, -135.0f));        // Right eye right corner
+    Model_Points.push_back(cv::Point3d(-150.0f, -150.0f, -125.0f));      // Left Mouth corner
+    Model_Points.push_back(cv::Point3d(150.0f, -150.0f, -125.0f));  
 
 }
 
